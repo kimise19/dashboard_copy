ebd18cbb403c65d835c144a230ba3bee35e24011
tom mise <kimise@espe.edu.ec>
2024-06-04 21:32:09 -0500

    add category and modal & alerts

---
+++ src/components/Alert.jsx
@@ -0,0 +1,52 @@
+// src/services/alertService.js
+import Swal from 'sweetalert2';
+
+export const showDeleteConfirmation = async (callback) => {
+    return Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¡No podrás revertir esto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminarlo',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            callback();
+        }
+    });
+};
+
+export const showEditConfirmation = async (callback) => {
+    return Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¡Se modificará el producto!",
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, editar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            callback();
+        }
+    });
+};
+
+export const showSuccessAlert = (message) => {
+    return Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: message
+    });
+};
+
+export const showErrorAlert = (message) => {
+    return Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: message
+    });
+};
